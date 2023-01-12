a2e00b2ebf495c2dbd79c74ee9195a4c8373fe95
“toledojm” <toledojm@outlook.com>
2023-01-12 09:15:49 -0300

    grupo2

--- dataset.py
+++ dataset.py
@@ -2,6 +2,6 @@ from roboflow import Roboflow
 
 def importar():
 
-    rf = Roboflow(api_key="KOrKgmkGRHqzxP6SuKTp") #se crea la conexion
-    project = rf.workspace("asdasd-boz3q").project("glass-xrghl") # se busca el dataset del proyecto xx de espacio de trabajo ww
-    dataset = project.version(3).download("yolov8") # se descarga a carpeta local
+    #rf = Roboflow(api_key="KOrKgmkGRHqzxP6SuKTp") #se crea la conexion
+    #project = rf.workspace("prueba2-8v055").project("waste-jrhiu") # se busca el dataset del proyecto xx de espacio de trabajo ww
+    #dataset = project.version(2).download("yolov8") # se descarga a carpeta local
--- train.py
+++ train.py
@@ -5,6 +5,6 @@ def entrenar():
     model = YOLO("yolov8n.yaml")  # build a new model from scratch
 
 # Use the model
-    results = model.train(data="Glass-3/data.yaml", epochs=1, batch=1, imgsz=640)  # train the model salida best.pt
+    results = model.train(data="Glass-3/data.yaml", epochs=1, batch=1, imgsz=640,__cached__=True )  # train the model salida best.pt
     results = model.val("Glass-3/data.yaml")  # evaluate model performance on the validation set
 
