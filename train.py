a289bd38029c7ce7e162fba23f4530e19aa0f302
“toledojm” <toledojm@outlook.com>
2023-01-16 11:45:55 -0300

    jose

--- train.py
+++ train.py
@@ -5,6 +5,6 @@ def entrenar():
     model = YOLO("yolov8n.yaml")  # build a new model from scratch
 
 # Use the model
-    results = model.train(data="datasets/data.yaml", epochs=1, batch=1, imgsz=640,cache='ram')  # train the model salida best.pt
+    results = model.train(data="datasets/data.yaml", epochs=1, batch=12, imgsz=640,cache='ram')  # train the model salida best.pt
     results = model.val("datasets/data.yaml")  # evaluate model performance on the validation set
 
